54026ce369fdd72b889b1aa6a3f740ae0fa974ed
Pawel Halka <arktykahalka@gmail.com>
2024-10-24 18:08:49 +0200

    Add Exception For Input

--- Kalkulator.py
+++ Kalkulator.py
@@ -2,13 +2,29 @@
 import logging
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 
+def ifConvertable(value1, value2):
+    try:
+        int(value1)
+        int(value2)
+    except:
+        logging.error('Znaleziono slowo, Wprowadz same liczby')
 
+def TypeEquasion(value1):
+    if 0 < int(value1) < 5:
+        return True
+    else:
+        logging.error('Liczba spoza zakresu, Spruboj ponownie')
 
 if __name__ == "__main__":
-    type_operation = input('Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ')
-    first_number = input('Podaj pierwsza liczbe: ')
-    second_number = input('Podaj druga liczbe: ')
-
+    while True:
+        type_operation = input('Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ')
+        if TypeEquasion(type_operation) == True:
+            break
+    while True:
+        first_number = input('Podaj pierwsza liczbe: ')
+        second_number = input('Podaj druga liczbe: ')
+        if ifConvertable(first_number, second_number) == None:
+            break
     
     if type_operation == '1':
         result = int(first_number) + int(second_number)
