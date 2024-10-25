4b867a7329c1423ce9e5e8f144fbd7f2ad5f5049
Pawel Halka <arktykahalka@gmail.com>
2024-10-25 11:24:13 +0200

    Add Multi math operations for user

--- Kalkulator.py
+++ Kalkulator.py
@@ -21,51 +21,57 @@ def TypeEquasion(value1):
         logging.error('Niepoprawna komenda, Sprobuj podac liczbe z zakresu')
 
 numbers = []
-result = 1
 
 if __name__ == "__main__":
     while True:
-        type_operation = input('Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ')
-        if TypeEquasion(type_operation) == True:
-            break
-    if int(type_operation) == 3:
+        result = 1
         while True:
-            logging.info('Podaj liczby oddzielone przecinkiem!')
-            numbers = input('Podaj liczby: ').split(',')
-            if IsConvertable(numbers) == None:
+            type_operation = input('Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ')
+            if TypeEquasion(type_operation) == True:
                 break
-        numbers = list(map(int, numbers)) #dziala
-        iter_numbers = iter(numbers)
-        while True:
-            try:
-                multi_numbers = lambda x, y: x * y
-                result = multi_numbers(result, next(iter_numbers))
-            except:
-                break
-        logging.info(f'Wynik to: {result}')
-
-    elif int(type_operation) == 1:
-        while True:
-            logging.info('Podaj liczby oddzielone przecinkiem!')
-            numbers = input('Podaj liczby: ').split(',')
-            if IsConvertable(numbers) == None:
-                break
-        numbers = list(map(int, numbers)) #dziala
-        result = sum(numbers)
-        logging.info(f'Wynik to: {result}')
-
-    else:
-        while True:
-            numbers.append(input('Podaj pierwsza liczbe: '))
-            numbers.append(input('Podaj druga liczbe: '))
-            if IsConvertable(numbers) == None:
-                break
-    
-        if int(type_operation) == 4:
-            result = int(numbers[0]) / int(numbers[1])
-            logging.info(f'Dziele: {numbers[0]}, {numbers[1]}')
+        if int(type_operation) == 3:
+            while True:
+                logging.info('Podaj liczby oddzielone przecinkiem!')
+                numbers = input('Podaj liczby: ').split(',')
+                if IsConvertable(numbers) == None:
+                    break
+            numbers = list(map(int, numbers)) #dziala
+            iter_numbers = iter(numbers)
+            while True:
+                try:
+                    multi_numbers = lambda x, y: x * y
+                    result = multi_numbers(result, next(iter_numbers))
+                except:
+                    break
             logging.info(f'Wynik to: {result}')
-        elif int(type_operation) == 2:
-            result = int(numbers[0]) - int(numbers[1])
-            logging.info(f'Odejmuje: {numbers[0]}, {numbers[1]}')
+
+        elif int(type_operation) == 1:
+            while True:
+                logging.info('Podaj liczby oddzielone przecinkiem!')
+                numbers = input('Podaj liczby: ').split(',')
+                if IsConvertable(numbers) == None:
+                    break
+            numbers = list(map(int, numbers)) #dziala
+            result = sum(numbers)
             logging.info(f'Wynik to: {result}')
+
+        else:
+            while True:
+                numbers.append(input('Podaj pierwsza liczbe: '))
+                numbers.append(input('Podaj druga liczbe: '))
+                if IsConvertable(numbers) == None:
+                    break
+        
+            if int(type_operation) == 4:
+                result = int(numbers[0]) / int(numbers[1])
+                logging.info(f'Dziele: {numbers[0]}, {numbers[1]}')
+                logging.info(f'Wynik to: {result}')
+            elif int(type_operation) == 2:
+                result = int(numbers[0]) - int(numbers[1])
+                logging.info(f'Odejmuje: {numbers[0]}, {numbers[1]}')
+                logging.info(f'Wynik to: {result}')
+        answer = input('Czy chcesz liczyc dalej? T/N: ')
+        if answer == 'T':
+            pass
+        else:
+            exit()
