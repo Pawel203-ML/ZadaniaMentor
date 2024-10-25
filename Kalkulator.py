4b1fade28bfd918bd92b21ddc062aa87c21db6c7
Pawel Halka <arktykahalka@gmail.com>
2024-10-25 18:30:06 +0200

    Fix converting input to float

--- Kalkulator.py
+++ Kalkulator.py
@@ -7,13 +7,13 @@ logging.basicConfig(level=logging.INFO, format='%(message)s')
 def IsConvertable(numbers):
     try:
         for number in numbers:
-            int(number)
+            float(number)
     except:
         logging.error('Znaleziono slowo, uzyj tylko liczb')
 
 def TypeEquasion(value1):
     try:
-        if 0 < int(value1) < 5:
+        if 0 < float(value1) < 5:
             return True
         else:
             logging.error('Liczba spoza zakresu, Spruboj ponownie')
@@ -24,25 +24,32 @@ numbers = []
 
 if __name__ == "__main__":
     while True:
-        result = 1
+        result = 1.0
         while True:
             type_operation = input('Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ')
             if TypeEquasion(type_operation) == True:
                 break
         if int(type_operation) == 3:
             while True:
-                logging.info('Podaj liczby oddzielone przecinkiem!')
-                numbers = input('Podaj liczby: ').split(',')
-                if IsConvertable(numbers) == None:
+                while True:
+                    logging.info('Podaj liczby oddzielone przecinkiem!')
+                    numbers = input('Podaj liczby: ').split(',')
+                    if IsConvertable(numbers) == None:
+                        break
+                try:
+                    numbers = list(map(float, numbers)) #dziala
+                    iter_numbers = iter(numbers)
                     break
-            numbers = list(map(int, numbers)) #dziala
-            iter_numbers = iter(numbers)
+                except:
+                    logging.error('Nieprawidlowo wprowadzone dane, sprubuj ponownie')
+
             while True:
                 try:
                     multi_numbers = lambda x, y: x * y
                     result = multi_numbers(result, next(iter_numbers))
                 except:
                     break
+            logging.info(f'Mnoze liczby: {numbers}')
             logging.info(f'Wynik to: {result}')
 
         elif int(type_operation) == 1:
@@ -51,8 +58,9 @@ if __name__ == "__main__":
                 numbers = input('Podaj liczby: ').split(',')
                 if IsConvertable(numbers) == None:
                     break
-            numbers = list(map(int, numbers)) #dziala
+            numbers = list(map(float, numbers)) #dziala
             result = sum(numbers)
+            logging.info(f'Dodaje liczby: {numbers}')
             logging.info(f'Wynik to: {result}')
 
         else:
@@ -63,11 +71,11 @@ if __name__ == "__main__":
                     break
         
             if int(type_operation) == 4:
-                result = int(numbers[0]) / int(numbers[1])
+                result = float(numbers[0]) / float(numbers[1])
                 logging.info(f'Dziele: {numbers[0]}, {numbers[1]}')
                 logging.info(f'Wynik to: {result}')
             elif int(type_operation) == 2:
-                result = int(numbers[0]) - int(numbers[1])
+                result = float(numbers[0]) - float(numbers[1])
                 logging.info(f'Odejmuje: {numbers[0]}, {numbers[1]}')
                 logging.info(f'Wynik to: {result}')
         while True:
