3a1c588498c35d39c754524c5348411aa56e6641
Pawel Halka <arktykahalka@gmail.com>
2024-10-30 22:52:27 +0100

    Add __str__ for all class

--- Inheritance.py
+++ Inheritance.py
@@ -9,6 +9,9 @@ class AddressBook:
         self.company = company
         self.mail = mail
 
+    def __str__(self):
+        return f'{self.name} {self.surname}'
+
 class BaseContact(AddressBook):
     def __init__(self,priv_phone, *args, **kwargs):
         super().__init__(*args,**kwargs)
@@ -17,6 +20,9 @@ class BaseContact(AddressBook):
 
     def contact(self):
         return f'Wybieram numer {self.priv_phone} i dzwonie do {self.name} {self.surname}'
+    
+    def __str__(self):
+        return f'{self.priv_phone} {self.mail}'
 
 
 class BusinessContact(AddressBook):
@@ -26,7 +32,16 @@ class BusinessContact(AddressBook):
 
     def contact(self):
         return f'Wybieram numer {self.business_phone} i dzwonie do {self.name} {self.surname}'
+    
+    def __str__(self):
+        return f'{self.business_phone} {self.company}'
 
 
 BaseContactList = []
 BusinessContactList =[]
+
+#dodawanie wartosci do list
+for i in range(5):
+    BaseContactList.append(BaseContact(name= fake.name(), surname= fake.last_name(), job= fake.job(), company= fake.company(), mail= fake.email(), priv_phone= fake.phone_number()))
+    BusinessContactList.append(BusinessContact(name= fake.name(), surname= fake.last_name(), job= fake.job(), company= fake.company(), mail= fake.email(), business_phone= fake.phone_number()))
+
