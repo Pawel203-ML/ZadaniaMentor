3b6eed8557457e9f42eba85e3e0d91cc637a563f
Pawel Halka <arktykahalka@gmail.com>
2024-11-03 13:34:05 +0100

    Add classes for movies and series

---
+++ Movies_Library.py
@@ -0,0 +1,18 @@
+class BaseInfo:
+    def __init__(self, title, release_date, category, num_views):
+        self.title = title
+        self.release_date = release_date
+        self.category = category
+        self.num_views = num_views
+
+class Movie(BaseInfo):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, *kwargs)
+
+class Series(BaseInfo):
+    def __init__(self, num_odc, num_season, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.num_odc = num_odc
+        self.num_season = num_season
+
+Movies_n_Series = []
